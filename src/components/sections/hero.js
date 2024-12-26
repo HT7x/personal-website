23bd3e61efc8fab73168b14f4eb3d32883b07add
HT7x <ht7.bhr@gmail.com>
2024-12-26 14:09:39 +0300

    resume uploaded

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -64,15 +64,7 @@ const Hero = () => {
   // const three = <h3 className="big-heading">I build things for the web.</h3>;
   const three = (
     <>
-      <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
-      </p>
+      <p>I'm a Senior Computer Science Student at Columbia University.</p>
     </>
   );
 
